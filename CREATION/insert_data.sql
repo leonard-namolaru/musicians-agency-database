86ee517305cab2ada000ce3b49db505335c03925
namolaru <leonard.namolaru@yahoo.com>
2022-04-27 19:41:27 +0200

    Modification des chemins pour éviter l'erreur Permission denied

--- CREATION/insert_data.sql
+++ CREATION/insert_data.sql
@@ -3,15 +3,15 @@
 --> Do not change the csv files name as they match the tables thus the below commands
 --> Execute the commands the exact same order 
 
-COPY musicien FROM '/Users/henchir/Desktop/Master 1 LP/Semestre 2/Bdd Avancé/Projet /script sql /sources csv/table musicien.csv' CSV HEADER; --1000 rows
-COPY agent FROM '/Users/henchir/Desktop/Master 1 LP/Semestre 2/Bdd Avancé/Projet /script sql /sources csv/table agent.csv' CSV HEADER; --1000 rows
-COPY producteur FROM '/Users/henchir/Desktop/Master 1 LP/Semestre 2/Bdd Avancé/Projet /script sql /sources csv/table producteur.csv' CSV HEADER; --1000 rows
-COPY instrument FROM '/Users/henchir/Desktop/Master 1 LP/Semestre 2/Bdd Avancé/Projet /script sql /sources csv/table instrument.csv' CSV HEADER; --6 rows
-COPY style_musique FROM '/Users/henchir/Desktop/Master 1 LP/Semestre 2/Bdd Avancé/Projet /script sql /sources csv/table style_musique.csv' CSV HEADER; --6 rows
-COPY contrat_artiste_producteur FROM '/Users/henchir/Desktop/Master 1 LP/Semestre 2/Bdd Avancé/Projet /script sql /sources csv/table contrat_artiste_producteur.csv' CSV HEADER; --100 rows
-COPY demande FROM '/Users/henchir/Desktop/Master 1 LP/Semestre 2/Bdd Avancé/Projet /script sql /sources csv/table demande.csv' CSV HEADER; --50 rows
-COPY contrat_agent_artiste FROM '/Users/henchir/Desktop/Master 1 LP/Semestre 2/Bdd Avancé/Projet /script sql /sources csv/table contrat_agent_artiste.csv' CSV HEADER;--100 rows
-COPY paiement_artiste FROM '/Users/henchir/Desktop/Master 1 LP/Semestre 2/Bdd Avancé/Projet /script sql /sources csv/table paiement_artiste.csv' CSV HEADER;--100 rows ( matching the contrats)
-COPY albums FROM '/Users/henchir/Desktop/Master 1 LP/Semestre 2/Bdd Avancé/Projet /script sql /sources csv/table album.csv' CSV HEADER; --30
-COPY joue FROM '/Users/henchir/Desktop/Master 1 LP/Semestre 2/Bdd Avancé/Projet /script sql /sources csv/table joue.csv' CSV HEADER;--1000 rows (matching the musicians)
-COPY maitrise FROM '/Users/henchir/Desktop/Master 1 LP/Semestre 2/Bdd Avancé/Projet /script sql /sources csv/table maitrise.csv' CSV HEADER; --1000 rows (matching the musicians)
+COPY musicien FROM 'C:\Users\Public\Csv files\table musicien.csv' CSV HEADER; --1000 rows
+COPY agent FROM 'C:\Users\Public\Csv files\table agent.csv' CSV HEADER; --1000 rows
+COPY producteur FROM 'C:\Users\Public\Csv files\table producteur.csv' CSV HEADER; --1000 rows
+COPY instrument FROM 'C:\Users\Public\Csv files\table instrument.csv' CSV HEADER; --6 rows
+COPY style_musique FROM 'C:\Users\Public\Csv files\table style_musique.csv' CSV HEADER; --6 rows
+COPY contrat_artiste_producteur FROM 'C:\Users\Public\Csv files\table contrat_artiste_producteur.csv' CSV HEADER; --100 rows
+COPY demande FROM 'C:\Users\Public\Csv files\table demande.csv' CSV HEADER; --50 rows
+COPY contrat_agent_artiste FROM 'C:\Users\Public\Csv files\table contrat_agent_artiste.csv' CSV HEADER;--100 rows
+COPY paiement_artiste FROM 'C:\Users\Public\Csv files\table paiement_artiste.csv' CSV HEADER;--100 rows ( matching the contrats)
+COPY albums FROM 'C:\Users\Public\Csv files\table album.csv' CSV HEADER; --30
+COPY joue FROM 'C:\Users\Public\Csv files\table joue.csv' CSV HEADER;--1000 rows (matching the musicians)
+COPY maitrise FROM 'C:\Users\Public\Csv files\table maitrise.csv' CSV HEADER; --1000 rows (matching the musicians)
